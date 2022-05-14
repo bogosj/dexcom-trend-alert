de4233d4d70fb0e65838e659bca772050ecc1fd7
James Bogosian <bogosj@users.noreply.github.com>
2022-05-14 20:32:58 +0000

    Use logging instead of print().

--- alert.py
+++ alert.py
@@ -1,6 +1,20 @@
-import apprise
+import argparse
+import logging
 import os
+
+import apprise
 import pydexcom
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-log',
+    '--loglevel',
+    default='info',
+    help='Provide logging level. Example --loglevel debug, default=info')
+
+args = parser.parse_args()
+
+logging.basicConfig(level=args.loglevel.upper())
 
 _DISPLAY_NAME = os.environ['DISPLAY_NAME']
 _USERNAME = os.environ['DEXCOM_USERNAME']
@@ -9,7 +23,8 @@ _NOTIFICATION = os.environ['NOTIFICATION_URI']
 
 dexcom = pydexcom.Dexcom(_USERNAME, _PASSWORD)
 bg = dexcom.get_current_glucose_reading()
-print(bg.trend_description)
+logging.info("Glucose reading: %s, trend: %s, time: %s",
+             bg.mg_dl, bg.trend_description, bg.time)
 
 note = apprise.Apprise()
 note.add(_NOTIFICATION)
