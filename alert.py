5dc9152f1a9ea5c50426fb32c2961c69cb1e15ed
James Bogosian <bogosj@users.noreply.github.com>
2022-05-15 17:51:43 +0000

    Setup infinite loop and delayed checks.

--- alert.py
+++ alert.py
@@ -1,8 +1,10 @@
 import argparse
+import datetime
 import logging
 import os
 
 import apprise
+import pause
 import pydexcom
 
 parser = argparse.ArgumentParser()
@@ -22,12 +24,21 @@ _PASSWORD = os.environ['DEXCOM_PASSWORD']
 _NOTIFICATION = os.environ['NOTIFICATION_URI']
 
 dexcom = pydexcom.Dexcom(_USERNAME, _PASSWORD)
-bg = dexcom.get_current_glucose_reading()
-logging.info("Glucose reading: %s, trend: %s, time: %s",
-             bg.mg_dl, bg.trend_description, bg.time)
 
-note = apprise.Apprise()
-note.add(_NOTIFICATION)
-note.notify(
-    body=f"{_DISPLAY_NAME} glucose is {bg.trend_description} at {bg.mg_dl}"
-)
+while True:
+    bg = dexcom.get_current_glucose_reading()
+    logging.info("Glucose reading: %s, trend: %s, time: %s",
+                bg.mg_dl, bg.trend_description, bg.time)
+
+    if bg.trend in (1,2,6,7):
+        note = apprise.Apprise()
+        note.add(_NOTIFICATION)
+        note.notify(
+            body=f"{_DISPLAY_NAME} glucose is {bg.trend_description} at {bg.mg_dl}"
+        )
+        pause.hours(1)
+    else:
+        next_check = bg.time + datetime.timedelta(minutes=5, seconds=30)
+        if next_check < datetime.datetime.now():
+            next_check = datetime.datetime.now() + datetime.timedelta(minutes=1)
+        pause.until(next_check)
